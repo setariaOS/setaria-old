ee375eaac5dfeeed44571b6f0cf8d640652226f4
kmc7468 <kmc7468@naver.com>
2017-11-17 19:58:49 +0900

    Updated BIOS BootLoader - Changed structure

--- setaria/src/boot/bootloader.asm
+++ setaria/src/boot/bootloader.asm
@@ -1,87 +1,163 @@
 [ORG 0x00]
 [BITS 16]
 
-SECTION .text
+section .text
 
 jmp 0x07C0:bootloader_start
 
 bootloader_start:
-	mov ax, 0x07C0
-	mov ds, ax
-	mov ax, 0xB800
-	mov es, ax
+    mov ax, 0x07C0
+    mov ds, ax
+    mov ax, 0xB800
+    mov es, ax
+    
+    mov si, 0
 
-	mov ax, 0x0000
-	mov ss, ax
-	mov sp, 0xFFFE
-	mov bp, 0xFFFE
+bootloader_stack_reset:
+    mov ax, 0x0000
+    mov ss, ax
+    mov sp, 0xFFFE
+    mov bp, 0xFFFE
 
-	mov si, 0
-	jmp screen_clear
+bootloader_screen_clear:
+    mov byte[es:si], 0
+    mov byte[es:si + 1], 0x07
+    add si, 2
 
-bootloader_enable_protected:
-	jmp $
+    cmp si, 80 * 25 * 2
+    jl bootloader_screen_clear
 
-screen_clear:
-	mov byte[es:si], 0
-	mov byte[es:si + 1], 0x0F
-	add si, 2
-	cmp si, 80 * 25 * 2
-	jl screen_clear
+    push bootloader_message_boot_started
+    push word[bootloader_message_y]
+    push 0
+    call bootloader_screen_print
+    add word[bootloader_message_y], 0x01
+    add sp, 6
 
-	push message_boot_start
-	push 0
-	push 0
-	call screen_print
-	add sp, 6
+bootloader_disk_reset:
+    mov ax, 0
+    mov dl, 0
+    int 0x13
+    jc bootloader_bios_exception
 
-	jmp bootloader_enable_protected
+    mov si, 0x1000
+    mov es, si
+    mov bx, 0x0000
+    mov di, word[bootloader_micro_kernel_size]
 
-screen_print:
-	push bp
-	mov bp, sp
-	push es
-	push si
-	push di
-	push ax
-	push cx
-	push dx
+    push bootloader_message_disk_reset
+    push word[bootloader_message_y]
+    push 0
+    call bootloader_screen_print
+    add word[bootloader_message_y], 0x01
+    add sp, 6
 
-	mov ax, 0xB800
-	mov es, ax
+bootloader_disk_read:
+    cmp di, 0
+    je bootloader_disk_read_end
+    sub di, 0x01
 
-	mov ax, word[bp + 6]
-	mov si, 160
-	mul si
-	mov di, ax
-	mov ax, word[bp + 4]
-	mov si, 2
-	mul si
-	add di, ax
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, byte[bootloader_micro_kernel_start_track]
+    mov cl, byte[bootloader_micro_kernel_start_sector]
+    mov dh, byte[bootloader_micro_kernel_start_head]
+    mov dl, 0x00
+    int 0x13
+    jc bootloader_bios_exception
 
-	mov si, word[bp + 8]
+    add si, 0x0020
+    mov es, si
 
-screen_draw:
-	mov cl, byte[si]
-	cmp cl, 0
-	je screen_draw_end
-	mov byte[es:di], cl
-	add si, 1
-	add di, 2
-	jmp screen_draw
+    add byte[bootloader_micro_kernel_start_sector], 0x01
+    cmp al, 19
+    jl bootloader_disk_read
 
-screen_draw_end:
-	pop dx
-	pop cx
-	pop ax
-	pop di
-	pop si
-	pop es
-	pop bp
-	ret
+    xor byte[bootloader_micro_kernel_start_head], 0x01
+    mov byte[bootloader_micro_kernel_start_sector], 0x01
+    cmp byte[bootloader_micro_kernel_start_head], 0x00
+    jne bootloader_disk_read
 
-message_boot_start: db 'Booting start', 0
+    add byte[bootloader_micro_kernel_start_track], 0x01
+    jmp bootloader_disk_read
 
-times 510 - ($ - $$) db 0x00
+bootloader_disk_read_end:
+    push bootloader_message_micro_kernel_read
+    push word[bootloader_message_y]
+    push 0
+    call bootloader_screen_print
+    add word[bootloader_message_y], 0x01
+    add sp, 6
+    
+bootloader_start_micro_kernel:
+    jmp 0x1000:0x0000
+
+bootloader_bios_exception:
+    push bootloader_message_bios_exception
+    push word[bootloader_message_y]
+    push 0
+    call bootloader_screen_print
+    add word[bootloader_message_y], 0x01
+    add sp, 6
+
+    jmp $
+
+bootloader_screen_print:
+    push bp
+    mov bp, sp
+    push es
+    push si
+    push di
+    push ax
+    push cx
+    push dx
+
+    mov ax, 0xB800
+    mov es, ax
+
+    mov ax, word[bp + 6]
+    mov si, 160
+    mul si
+    mov di, ax
+
+    mov ax, word[bp + 4]
+    mov si, 2
+    mul si
+    add di, ax
+
+    mov si, word[bp + 8]
+
+bootloader_screen_print_loop:
+    mov cl, byte[si]
+    cmp cl, 0
+    je bootloader_screen_print_end
+    
+    mov byte[es:di], cl
+    add si, 1
+    add di, 2
+    
+    jmp bootloader_screen_print_loop
+
+bootloader_screen_print_end:
+    pop dx
+    pop cx
+    pop ax
+    pop di
+    pop si
+    pop es
+    pop bp
+    ret
+
+bootloader_message_boot_started:        db '[BOOTLOADER] Boot has started.', 0x00
+bootloader_message_disk_reset:          db '[BOOTLOADER] Disk is ready to read.', 0x00
+bootloader_message_micro_kernel_read:   db '[BOOTLOADER] Micro-Kernel has loaded.', 0x00
+bootloader_message_bios_exception:      db '[BOOTLOADER] Unknown exception occurred in BIOS.', 0x00
+bootloader_message_y:                   dw 0x00
+bootloader_micro_kernel_size:           dw 0x0400
+bootloader_micro_kernel_start_head:     db 0x00
+bootloader_micro_kernel_start_track:    db 0x00
+bootloader_micro_kernel_start_sector:   db 0x02
+
+times 510 - ($ - $$)                    db 0x00
 db 0x55
 db 0xAA

b4afd2267f89828ae970cbe43f7a4cfd363b0a77
kmc7468 <kmc7468@naver.com>
2017-11-19 22:09:53 +0900

    Updated x86_64 BIOS BootLoader - Implemented bios exception handler

--- setaria/boot/x86_64/bootloader.asm
+++ setaria/boot/x86_64/bootloader.asm
@@ -3,9 +3,9 @@
 
 section .text
 
-jmp 0x07C0:bootloader_start
+jmp 0x07C0:bootloader16_start
 
-bootloader_start:
+bootloader16_start:
 	mov ax, 0x07C0
 	mov ds, ax
 	mov ax, 0xB800
@@ -13,32 +13,50 @@ bootloader_start:
 
 	mov si, 0
 
-bootloader_stack_reset:
-	mov ax, 0x0000
-	mov ss, ax
-	mov sp, 0xFFFE
-	mov bp, 0xFFFE
-
-bootloader_screen_clear:
+bootloader16_screen_clear:
 	mov byte[es:si], 0
 	mov byte[es:si + 1], 0x07
 	add si, 2
 
 	cmp si, 80 * 25 * 2
-	jl bootloader_screen_clear
+	jl bootloader16_screen_clear
 	
 	mov ah, 0x02
 	mov bh, 0x00
 	mov dh, 0x00
 	mov dl, 0x00
 	int 0x10
-	jc bootloader_bios_exception
+	jc bootloader16_bios_exception
 
+	jmp bootloader16_wait
+
+bootloader16_bios_exception:
+	mov di, 0
+	mov si, 0
+
+bootloader16_bios_exception_loop:
+	mov cl, byte[bootloader16_message_bios_exception + si]
+	cmp cl, 0
+	je bootloader16_bios_exception_loop_end
+
+	mov byte[es:di], cl
+	add si, 1
+	add di, 2
+
+	jmp bootloader16_bios_exception_loop
+
+bootloader16_bios_exception_loop_end:
+	mov ah, 0x02
+	mov bh, 0x00
+	mov dh, 0x00
+	mov dl, 44
+	int 0x10
+
+bootloader16_wait:
 	jmp $
 
-bootloader_bios_exception:
-	jmp $
+bootloader16_message_bios_exception: 	db '[setaria] An error has occurred in the BIOS.', 0x00
 
-times 510 - ($ - $$) db 0x00
+times 510 - ($ - $$) 				db 0x00
 db 0x55
 db 0xAA

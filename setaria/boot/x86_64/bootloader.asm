cbc6194f6158dcd3169c2f3bb20330930224d76e
kmc7468 <kmc7468@naver.com>
2017-11-22 18:22:22 +0900

    Updated x86_64 BIOS BootLoader - Changed labels' name

--- setaria/boot/x86_64/bootloader.asm
+++ setaria/boot/x86_64/bootloader.asm
@@ -2,9 +2,9 @@
 [BITS 16]
 section .text
 
-jmp 0x07C0:bootloader16_start
+jmp 0x07C0:bootloader16.start
 
-bootloader16_start:
+bootloader16.start:
 	mov ax, 0x07C0
 	mov ds, ax
 	mov ax, 0xB800
@@ -12,35 +12,33 @@ bootloader16_start:
 
 	mov si, 0
 
-bootloader16_screen_clear:
+	mov ah, 0x01
+	mov ch, 0x3F
+	int 0x10
+
+bootloader16.screen.clear:
 	mov byte[es:si], 0
 	mov byte[es:si + 1], 0x07
 	add si, 2
 
 	cmp si, 80 * 25 * 2
-	jl bootloader16_screen_clear
+	jl bootloader16.screen.clear
 
-	mov ah, 0x02
-	mov bh, 0x00
-	mov dh, 0x00
-	mov dl, 0x00
-	int 0x10
-
-bootloader16_enable_a20:
+bootloader16.enable.a20:
 	mov ax, 0x2401
 	int 0x15
-	jc bootloader16_enable_a20_with_scp
-	jmp bootloader16_enable_protected_mode
+	jc bootloader16.enable.a20.with_scp
+	jmp bootloader16.enable.protected_mode
 
-bootloader16_enable_a20_with_scp:
+bootloader16.enable.a20.with_scp:
 	in al, 0x92
 	or al, 0x02
 	and al, 0xFE
 	out 0x92, al
 
-bootloader16_enable_protected_mode:
+bootloader16.enable.protected_mode:
 	cli
-	lgdt [bootloader32_gdtr]
+	lgdt [bootloader32.gdtr]
 
 	mov eax, cr0
 	or eax, 0x00000001
@@ -50,10 +48,10 @@ bootloader16_enable_protected_mode:
 	nop
 	nop
 
-	jmp dword 0x00000008:(bootloader32_start + 0x7C00)
+	jmp dword 0x00000008:(bootloader32.start + 0x7C00)
 
 [BITS 32]
-bootloader32_start:
+bootloader32.start:
 	mov ax, 0x0010
 	mov ds, ax
 	mov es, ax
@@ -62,11 +60,11 @@ bootloader32_start:
 
 	jmp $
 
-bootloader32_gdtr:
-	dw bootloader32_gdt_end - bootloader32_gdt - 1
-	dd bootloader32_gdt + 0x7C00
+bootloader32.gdtr:
+	dw bootloader32.gdt.end - bootloader32.gdt - 1
+	dd bootloader32.gdt + 0x7C00
 
-bootloader32_gdt:
+bootloader32.gdt:
 	dw 0x0000 ; Null
 	dw 0x0000
 	db 0x00
@@ -87,7 +85,7 @@ bootloader32_gdt:
 	db 0x92
 	db 0xCF
 	db 0x00
-bootloader32_gdt_end:
+bootloader32.gdt.end:
 
 times 510 - ($ - $$) db 0x00
 db 0x55

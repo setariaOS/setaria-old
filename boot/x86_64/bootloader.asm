83bd0d61c996113e71dacfce6d1053c75c4dae8a
kmc7468 <kmc7468@naver.com>
2017-11-27 21:09:11 +0900

    Updated x86_64 BIOS BootLoader and Kernel entry point - Changed lables' name

--- boot/x86_64/bootloader.asm
+++ boot/x86_64/bootloader.asm
@@ -2,9 +2,9 @@
 [BITS 16]
 section .text
 
-jmp word 0x07C0:bootloader16.start
+jmp word 0x07C0:bootloader16_start
 
-bootloader16.start:
+bootloader16_start:
 	mov ax, 0x07C0
 	mov ds, ax
 	mov ax, 0xB800
@@ -16,15 +16,15 @@ bootloader16.start:
 	mov ch, 0x3F
 	int 0x10
 
-bootloader16.screen.clear:
+bootloader16_screen_clear:
 	mov byte[es:si], 0
 	mov byte[es:si + 1], 0x07
 	add si, 2
 
 	cmp si, 80 * 25 * 2
-	jl bootloader16.screen.clear
+	jl bootloader16_screen_clear
 
-bootloader16.disk.read:
+bootloader16_disk_read:
 	mov ax, 0x0800
 	mov es, ax
 	mov bx, 0x0000
@@ -34,15 +34,15 @@ bootloader16.disk.read:
 	mov ch, 0x00
 	mov dh, 0x00
 	mov dl, 0x00
-	mov cl, byte[bootloader16.variable.sector]
+	mov cl, byte[bootloader16_variable_sector]
 	int 0x13
 	jc .error
-	add byte[bootloader16.variable.sector], 1
+	add byte[bootloader16_variable_sector], 1
 
 	add bx, 0x0200
 
-	cmp byte[bootloader16.variable.sector], 10
-	je bootloader16.a20.enable
+	cmp byte[bootloader16_variable_sector], 10
+	je bootloader16_a20_enable
 
 	jmp .loop
 .error:
@@ -50,32 +50,32 @@ bootloader16.disk.read:
 	mov es, ax
 
 	mov di, 0
-	mov si, bootloader16.message.cannot_read_memory
-.error.loop:
+	mov si, bootloader16_message_cannot_read_memory
+.error_loop:
 	lodsb
 	or al, al
-	jz .error.end
+	jz .error_end
 
 	mov byte[es:di], al
 	add di, 2
-	jmp .error.loop
-.error.end:
+	jmp .error_loop
+.error_end:
 	jmp $
 
-bootloader16.a20.enable:
+bootloader16_a20_enable:
 	mov ax, 0x2401
 	int 0x15
 	jc .with_scp
-	jmp bootloader16.protected_mode.enable
+	jmp bootloader16_protected_mode_enable
 .with_scp:
 	in al, 0x92
 	or al, 0x02
 	and al, 0xFE
 	out 0x92, al
 
-bootloader16.protected_mode.enable:
+bootloader16_protected_mode_enable:
 	cli
-	lgdt [bootloader32.gdtr]
+	lgdt [bootloader32_gdtr]
 
 	mov eax, cr0
 	or eax, 0x00000001
@@ -85,13 +85,13 @@ bootloader16.protected_mode.enable:
 	nop
 	nop
 
-	jmp dword 0x00000008:(bootloader32.start + 0x7C00)
+	jmp dword 0x00000008:(bootloader32_start + 0x7C00)
 
-bootloader16.message.cannot_read_memory: db 'Error1', 0x00
-bootloader16.variable.sector: db 2
+bootloader16_message_cannot_read_memory: db 'Error1', 0x00
+bootloader16_variable_sector: db 2
 
 [BITS 32]
-bootloader32.start:
+bootloader32_start:
 	mov ax, 0x0010
 	mov ds, ax
 	mov fs, ax
@@ -100,24 +100,24 @@ bootloader32.start:
 	mov ax, 0x0018
 	mov es, ax
 
-bootloader32.stack.reset:
+bootloader32_stack_reset:
 	mov ax, 0x0010
 	mov ss, ax
 
 	mov ebp, 0x8000
 	mov esp, 0x8000
 
-bootloader32.memory.size.check:
+bootloader32_memory_size_check:
 	mov eax, 1024 * 1024 * 32 - 4
 	mov dword[eax], 0x12345678
 	cmp dword[eax], 0x12345678
-	je bootloader32.long_mode.check
+	je bootloader32_long_mode_check
 .print:
-	push bootloader32.message.not_enough_memory + 0x7C00
-	call bootloader32.function.string.print
+	push bootloader32_message_not_enough_memory + 0x7C00
+	call bootloader32_function_string_print
 	add esp, 4
 
-bootloader32.long_mode.check:
+bootloader32_long_mode_check:
 	mov eax, 0x80000000
 	cpuid
 	cmp eax, 0x80000001
@@ -128,13 +128,13 @@ bootloader32.long_mode.check:
 	test edx, 1 << 29
 	jz .print
 
-	jmp bootloader32.page.create
+	jmp bootloader32_page_create
 .print:
-	push bootloader32.message.not_supported_long_mode + 0x7C00
-	call bootloader32.function.string.print
+	push bootloader32_message_not_supported_long_mode + 0x7C00
+	call bootloader32_function_string_print
 	add esp, 4
 
-bootloader32.page.create:
+bootloader32_page_create:
 	mov edi, 0x00009000
 	mov dword[ds:edi], 0x0000A000 | (0x00000001 | 0x00000002)
 	mov dword[ds:edi + 4], 0
@@ -147,7 +147,7 @@ bootloader32.page.create:
 	mov dword[ds:edi], 0x00000001 | 0x00000002 | 0x00000080
 	mov dword[ds:edi + 4], 0
 
-bootloader32.long_mode.enable:
+bootloader32_long_mode_enable:
 	mov eax, cr4
 	or eax, 0x00000020
 	mov cr4, eax
@@ -169,12 +169,12 @@ bootloader32.long_mode.enable:
 	nop
 	nop
 
-	lgdt [bootloader64.gdtr + 0x7C00]
+	lgdt [bootloader64_gdtr + 0x7C00]
 
 	jmp dword 0x00000008:0x8000
 
 ; Arguments: Message Address
-bootloader32.function.string.print:
+bootloader32_function_string_print:
 	push ebp
 	mov ebp, esp
 	push edi
@@ -196,11 +196,11 @@ bootloader32.function.string.print:
 	pop ebp
 	ret
 
-bootloader32.gdtr:
-	dw bootloader32.gdt.end - bootloader32.gdt - 1
-	dd bootloader32.gdt + 0x7C00
+bootloader32_gdtr:
+	dw bootloader32_gdt_end - bootloader32_gdt - 1
+	dd bootloader32_gdt + 0x7C00
 
-bootloader32.gdt:
+bootloader32_gdt:
 	dw 0x0000	; Null
 	dw 0x0000
 	db 0x00
@@ -228,17 +228,17 @@ bootloader32.gdt:
 	db 0x92
 	db 0xC0
 	db 0x00
-bootloader32.gdt.end:
+bootloader32_gdt_end:
 
-bootloader32.message.not_enough_memory: db 'Error2', 0x00
-bootloader32.message.not_supported_long_mode: db 'Error3', 0x00
+bootloader32_message_not_enough_memory: db 'Error2', 0x00
+bootloader32_message_not_supported_long_mode: db 'Error3', 0x00
 
 [BITS 64]
-bootloader64.gdtr:
-	dw bootloader64.gdt.end - bootloader64.gdt - 1
-	dd bootloader64.gdt + 0x7C00
+bootloader64_gdtr:
+	dw bootloader64_gdt_end - bootloader64_gdt - 1
+	dd bootloader64_gdt + 0x7C00
 
-bootloader64.gdt:
+bootloader64_gdt:
 	dw 0x0000	; Null
 	dw 0x0000
 	db 0x00
@@ -259,7 +259,7 @@ bootloader64.gdt:
 	db 0x92
 	db 0xAF
 	db 0x00
-bootloader64.gdt.end:
+bootloader64_gdt_end:
 
 times 510 - ($ - $$) db 0x00
 db 0x55
--- kernel/entrypoint.asm
+++ kernel/entrypoint.asm
@@ -2,7 +2,7 @@
 [BITS 64]
 section .text
 
-kernel.entry_point:
+kernel_entry_point:
 	mov ax, 0x0010
 	mov ds, ax
 	mov es, ax
